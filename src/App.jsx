eda87961848ef60b0b59b042e422f580b85a174e
CleberSouza <souzacleber.cs.cs@gmail.com>
2025-11-22 15:53:42 -0300

    feat: implement App component with routing and layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-export default function App() {
-    return (
-        <h1>Hello React + Vite!</h1>
-    );
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header/Header";
+import Home from "./pages/Home";
+import Concepts from "./pages/Concepts";
+import WebApplication from "./pages/WebApplication";
+import Example from "./pages/Example";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/concepts" element={<Concepts />} />
+          <Route path="/web-application" element={<WebApplication />} />
+          <Route path="/example" element={<Example />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
+
+export default App;
